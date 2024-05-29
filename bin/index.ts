2e6d22663c0507d08cb656d1be5d82a660a562a5
samiyamarium <160397803+samiyamarium@users.noreply.github.com>
2024-05-29 15:57:22 +0500

    Create index.ts

---
+++ bin/index.ts
@@ -0,0 +1,29 @@
+#! /usr/bin/env node
+const inquirer=require('inquirer');
+const chalk=require('chalk')
+let q=inquirer.prompt([{
+    name:'num1',
+    type:'number',
+    message:'Enter first number',
+},
+    {name:'num2',
+    type:'number',
+    message:'Enter second number'
+},
+{
+    name:'calculate',
+    type:'checkbox',
+    message:'Select operation to perform',
+    choices:['Add','Subtract','Multiply','Divide']
+}])
+.then(answer=>{
+    if(answer.calculate=='Add'){
+console.log(chalk.green('Performed Addition: Answer=', answer.num1+answer.num2))}
+if(answer.calculate=='Subtract'){
+    console.log(chalk.yellow('Performed Subtraction: Answer=', answer.num1-answer.num2))}
+    if(answer.calculate=='Multiply'){
+        console.log(chalk.red('Performed Multiplication: Answer=', answer.num1*answer.num2))}
+        if(answer.calculate=='Divide'){
+            console.log(chalk.magenta('Perform Division: Answer=', answer.num1/answer.num2))}
+
+})
